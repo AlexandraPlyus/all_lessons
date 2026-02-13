b2ee5d36933552c688e096cc384f43847e0043e2
Александра <aleksandrapliusnina@gmail.com>
2026-02-13 17:05:41 +0500

    commit_lesson5

---
+++ 05_lesson/lesson05_task2.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+
+from selenium.webdriver.common.by import By
+from time import sleep
+
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+
+driver.get("http://uitestingplayground.com/dynamicid")
+
+driver.find_element(By.XPATH, "//button[text()='Button with Dynamic ID']").click()
+
+sleep(4)
---
+++ 05_lesson/lesson05_task4.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
+
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+
+driver.get("http://the-internet.herokuapp.com/login")
+
+username = driver.find_element(By.XPATH, "//*[@id='username']").send_keys("tomsmith")
+password = driver.find_element(By.CSS_SELECTOR, "#password").send_keys("SuperSecretPassword!")
+button_login = driver.find_element(By.CSS_SELECTOR, ".fa").click()
+logget = driver.find_element(By.CSS_SELECTOR, '#flash').text

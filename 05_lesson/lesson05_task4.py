d7bf50902dab4ea8bf99618ec1be5470bd3e513a
Александра <aleksandrapliusnina@gmail.com>
2026-02-13 18:59:11 +0500

    commit_lesson5

--- 05_lesson/lesson05_task4.py
+++ 05_lesson/lesson05_task4.py
@@ -12,3 +12,6 @@ username = driver.find_element(By.XPATH, "//*[@id='username']").send_keys("tomsm
 password = driver.find_element(By.CSS_SELECTOR, "#password").send_keys("SuperSecretPassword!")
 button_login = driver.find_element(By.CSS_SELECTOR, ".fa").click()
 logget = driver.find_element(By.CSS_SELECTOR, '#flash').text
+
+print(logget)
+driver.quit()

726c39f7d185aa17d06acff24e26a539f7bcff6e
Александра <aleksandrapliusnina@gmail.com>
2026-03-25 11:02:43 +0500

    COMMIT_08_LESSON

--- 08_lesson/test_08.py
+++ 08_lesson/test_08.py
@@ -1,21 +1,35 @@
 import pytest
 import requests
 import json
-from for_lesson_08 import KEY
+from ClassForLesson08 import GetKey
 
 url = "https://yougile.com"
+get_key_exemplar = GetKey(url)
+
+KEY = get_key_exemplar.get_key()
 
 def test_create_project():
     my_headers = {
         "Authorization": f"Bearer {KEY}"
     }
+
+    # Создать проект
     body = {
         "title": "ГосУслуги"
     }
     respone = requests.post(url+"/api-v2/projects", json=body, headers=my_headers)
-
+    respone_body = respone.json()
+    id = respone_body["id"]
     assert respone.status_code == 201
-
+    
+    # Удалить проект
+    body = {
+        "deleted": True
+    }
+    respone = requests.put(f"{url}/api-v2/projects/{id}", json=body, headers=my_headers)
+    assert respone.status_code == 200
+    
+    
 def test_create_project_no_title():
     my_headers = {
         "Authorization": f"Bearer {KEY}"
@@ -53,6 +67,13 @@ def test_rename_project():
     respone = requests.put(f"{url}/api-v2/projects/{id}", json=body, headers=my_headers)
     assert respone.status_code == 200
 
+    # Удалить проект
+    body = {
+        "deleted": True
+    }
+    respone = requests.put(f"{url}/api-v2/projects/{id}", json=body, headers=my_headers)
+    assert respone.status_code == 200
+
 def test_rename_project_empty_name():
 
     # Создать проект
@@ -81,6 +102,14 @@ def test_rename_project_empty_name():
     assert respone.status_code == 400
     assert body_respone["message"] == ["title should not be empty"]
 
+    # Удалить проект
+    body = {
+        "deleted": True
+    }
+    respone = requests.put(f"{url}/api-v2/projects/{id}", json=body, headers=my_headers)
+    assert respone.status_code == 200
+
+
 def test_get_by_id():
     
     title_project = "ГсУслуги"
@@ -95,12 +124,19 @@ def test_get_by_id():
     respone = requests.post(url+"/api-v2/projects", json=body, headers=my_headers)
     body_respone = respone.json()
 
-    id_project = body_respone["id"]
+    id = body_respone["id"]
 
     assert respone.status_code == 201
 
     # Получение по id
-    respone = requests.get(f"{url}/api-v2/projects/{id_project}", headers=my_headers)
+    respone = requests.get(f"{url}/api-v2/projects/{id}", headers=my_headers)
+    assert respone.status_code == 200
+
+    # Удалить проект
+    body = {
+        "deleted": True
+    }
+    respone = requests.put(f"{url}/api-v2/projects/{id}", json=body, headers=my_headers)
     assert respone.status_code == 200
 
 def test_get_by_empty_id():
@@ -116,18 +152,17 @@ def test_get_projects():
     assert respone.status_code == 200
 
 
-def test_delete_project():
-    id = "10e06011-5fe0-4dde-9040-189f9e624b35"
-    my_headers = {
-        "Authorization": f"Bearer {KEY}"
-    }
+# def test_delete_project():
+#     id = "1d8644e3-08e0-4500-b6cf-503f00e67576"
+#     my_headers = {
+#         "Authorization": f"Bearer {KEY}"
+#     }
     
-    body = {
-        "deleted": True
-    }
+#     body = {
+#         "deleted": True
+#     }
 
-    respone = requests.put(f"{url}/api-v2/projects/{id}", json=body, headers=my_headers)
-    assert respone.status_code == 200
+#     respone = requests.put(f"{url}/api-v2/projects/{id}", json=body, headers=my_headers)
+#     assert respone.status_code == 200
 
-  
 
